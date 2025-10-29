dcf8a5a80729410ab5fe3ce38a95f8435f8fab4b
Irene <isanarmu@gmail.com>
2025-10-29 12:05:22 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
+  let who = ['The dog', 'My grandma', 'The amazon delivery guy', 'A meteorite'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the computer', 'my house'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was playing'];
+
+  const randomNumber = arr => {
+    return Math.floor(Math.random() * arr.length)
+  }
+  let excuse = `${who[randomNumber(who)]} ${action[randomNumber(action)]} ${what[randomNumber(what)]} ${when[randomNumber(when)]}`
+   console.log(excuse)
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuse;
+  
+
 };
